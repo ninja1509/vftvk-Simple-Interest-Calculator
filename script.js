d237b06d092b5ef0d5aeff7dcde4e82a90b1cef0
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2021-10-11 08:04:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+// This is the javascript file
+// Compute funtion is created to do all the computational work for this calculator 
 function compute()
 {
-    pri = document.getElementById("principal").value;
+    //creating variables to perform operations
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value
+    var interest = principal * years * rate /100;
     
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //Conditional operation performed to check if the principle is valid or not
+    if(principal ==undefined || principal <=0){
+        alert("Enter a positive number");
+        principal.focus();
+    }
+    else{
+        var inr = "If you deposit <span class='highl'>" + principal+",</span><br>";
+        var ra = "at an interest rate of <span class='highl'>" + rate+"%,</span><br>";
+        var am = "You will receive an amount of <span class='highl'>" + interest+",</span><br>";
+        var ye = "in the year <span class='highl'>" + year +"</span>"; 
+        
+        document.getElementById("result").innerHTML=inr+ra+am+ye;
+    }
+
 }
         
+//This function is to update the rate value
+function updateRate(){
+
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
+}
+
+//This function is to validate and focus on 0 or -ve principal value.
+function validateAmount() { 
+    var principal = document.getElementById("principal").value; 
+    var lessThanZero = parseInt(principal) >= 0; 
+    if (!lessThanZero) {
+    document.getElementById("principal").focus();
+    document.getElementById("principal").select();
+    } 
+    }
